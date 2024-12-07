0383f6452435c4d6245dfddba6a6cfad9fa2a900
mudkipdev <mudkip@mudkip.dev>
2024-12-07 15:52:54 -0700

    add cli

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,42 @@
 pub mod parser;
 pub mod utility;
 
-use parser::Expression;
 use std::{
     io::{self, Write},
     process::exit,
 };
+use parser::Expression;
+use clap::Command;
 
 pub const VERSION: &str = "0.1.0";
 
 fn main() {
-    println!("Amber v{}\nType \"exit\" to quit.\n", VERSION);
+    let mut command = Command::new("amber")
+        .version(VERSION)
+        .author("mudkip")
+        .about("A high-level programming language written in Rust.")
+        .subcommand(
+            Command::new("shell")
+                .about("Launches an interactive shell to try out the language.")
+                .alias("repl")
+        );
+
+    let help = command.render_help();
+    let matches = command.get_matches();
+
+    match matches.subcommand() {
+        Some(("shell", _)) => launch_shell(),
+        _ => println!("{}", help)
+    }
+}
+
+fn launch_shell() {
+    println!(
+        "\n{}Amber v{}{}\nType \"exit\" to quit.\n",
+        "\x1b[1;32m", // bold red
+        VERSION,
+        "\x1b[0m" // reset
+    );
 
     loop {
         print!("> ");
